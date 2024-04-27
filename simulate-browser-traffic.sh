9b82e6279848e05e917ee2f04b7aa1814f67d536
Alex M <arm@Alexs-MBP.lan>
2024-04-27 10:08:07 +0200

    Updated logo

--- simulate-browser-traffic.sh
+++ simulate-browser-traffic.sh
@@ -1,5 +1,8 @@
-#docker run --network web-shop-o11y-demo_web-shop --rm -i grafana/k6  run -e HOSTNAME=${PUBLIC_APP_URL:=web-shop} - <shop-simulator/webshop-protocol-demo.js
+export PUBLIC_APP_URL="alexandermarquardt.info"
+echo $PUBLIC_APP_URL
+
+docker run --rm -i grafana/k6  run -e HOSTNAME=${PUBLIC_APP_URL:=web-shop} - <shop-simulator/webshop-protocol-demo.js
 #docker run --network web-shop-o11y-demo_web-shop --rm -i grafana/k6  run -e K6_BROWSER_ENABLED=true -e HOSTNAME=${PUBLIC_APP_URL:=web-shop} - <shop-simulator/webshop-browser-demo.js
 #docker run --network web-shop-o11y-demo_web-shop --rm -i grafana/k6  run -e K6_BROWSER_ENABLED=true -e HOSTNAME=${PUBLIC_APP_URL:=web-shop} - <shop-simulator/webshop-browser-demo.js
-docker run --rm --network web-shop-o11y-demo_web-shop -i --cap-add=SYS_ADMIN grafana/k6:master-with-browser run - < shop-simulator/webshop-browser-demo.js
+#docker run --rm -i --cap-add=SYS_ADMIN grafana/k6:master-with-browser run - < shop-simulator/webshop-browser-demo.js
 
--- simulate-user-traffic.sh
+++ simulate-user-traffic.sh
@@ -1,4 +1,4 @@
-export PUBLIC_APP_URL="4.156.201.184"
+export PUBLIC_APP_URL="alexandermarquardt.info"
 echo $PUBLIC_APP_URL
 docker run --rm -i grafana/k6  run -e HOSTNAME=${PUBLIC_APP_URL:=web-shop} - <shop-simulator/webshop-protocol-demo.js 
 docker run --rm -i grafana/k6  run -e HOSTNAME=${PUBLIC_APP_URL:=web-shop} - <shop-simulator/webshop-protocol-demo-phones.js 
